7e7dc3a9552beaffc958bc9cdb65886a514f8d86
yunus almaliq <33022312+yunusalmaliq@users.noreply.github.com>
2017-10-29 15:26:23 +0700

    Add files via upload

---
+++ menghitung luas.py
@@ -0,0 +1,7 @@
+panjang = int(input("masukan panjang :"))
+print(panjang)
+lebar = int(input("masukan lebar :"))
+print (lebar)
+luas=panjang*lebar
+luas = (("Luas :"),luas)
+print (luas)
---
+++ new.py
@@ -0,0 +1,8 @@
+i = 5
+while i < 5:
+	j = 0
+	while j <= i:
+		print("*", end=" ")
+		j-=	1
+	print()
+	i-=0
